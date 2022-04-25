8bb34a639e01f66b9c473fa1f01fd7ee5a4ebe3b
RedGuy12 <paul@reid-family.org>
2022-04-25 12:54:37 -0500

    unlint

--- commands/potato/leaderboard.js
+++ commands/potato/leaderboard.js
@@ -1,35 +1,27 @@
-
 "use strict";
-
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const Database = require("@replit/database");
-const { MessageEmbed, Permissions } = require("discord.js");
-
-const database = new Database(process.env.DB_URL);
-
-require("dotenv").config();
-
+const Database = require("@replit/database")
+const { MessageEmbed, Permissions } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const db = new Database(process.env.DB_URL)
+require("dotenv").config()
 async function run(inter) {
-  const users = await database.list();
-  const list = await Promise.all(users.filter((value) => !value.endsWith("-lastDaily")).map(async (id) => [id, await database.get(id)]));
-  const cutusers = list.sort(([, a], [, b]) => b - a).slice(0, 5);
+  let users = await db.list()
+  let list = await Promise.all(users.filter(val=> !val.endsWith("-lastDaily")).map(async (id)=>[id, await db.get(id)]))
+  let cutusers = list.sort(([,a], [,b]) => b - a).slice(0, 5)
   const exampleEmbed = new MessageEmbed()
-  .setColor("#da9c83")
-  .setTitle("/potato leaderboard")
-  .setAuthor({ name: inter.user.tag, iconURL: inter.user.displayAvatarURL() })
-  .setDescription(cutusers.map(([id, potatoes], index) => `${(index) + 1}. <@${id}> (${potatoes} 🥔)`).join("\n"))
+  .setColor('#da9c83')
+  .setTitle('/potato leaderboard')
+  .setAuthor({ name: inter.user.tag, iconURL: inter.user.displayAvatarURL()})
+  .setDescription(cutusers.map(([id, potatoes], index)=>`${(index)+1}. <@${id}> (${potatoes} 🥔)`).join("\n"))
     .setThumbnail("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/322/potato_1f954.png")
   .setTimestamp()
-  .setFooter({ text: `in #${inter.channel.name}` });
-
-  await inter.reply({ embeds: [exampleEmbed] });
+  .setFooter({ text: `in #${inter.channel.name}`});
+  await inter.reply({embeds:[exampleEmbed]})
 }
-
 const data = new SlashCommandBuilder()
-	.setName("leaderboard")
-	.setDescription("Shows top 5 potato collectors!");
-
+	.setName('leaderboard')
+	.setDescription('Shows top 5 potato collectors!')
 module.exports = {
-  meta: data,
-  run
-};
+  meta:data,
+  run:run
+}
