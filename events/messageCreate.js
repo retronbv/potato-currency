b3648dfe555c0fcfe8eb484916a05317477a40aa
Paul Reid <paul@reid-family.org>
2022-04-26 17:33:12 +0000

    fix logic

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,4 +1,5 @@
 function probability(n){
+	console.log(n)
   return Math.random() < n;
 }
 
@@ -6,10 +7,11 @@ let sinceLast = 0
 
 async function handler(msg) {
   if(msg.author.bot) return;
-  let messages = (await msg.channel.messages.fetch({limit: 10})).toJSON();
-  let spam = messages.filter((e) => e.author.bot || e.author.id == msg.author.id).length
+  let messages = (await msg.channel.messages.fetch({limit: 6})).toJSON();
+  let spam = messages.filter((e) => e.author.bot || e.author.id == msg.author.id).length+1
 
-	if (probability(0.01 *( (msg.content.length / 100)+1) * (sinceLast / 3) / (spam*10))) {
+	console.log(msg.content.length,sinceLast,spam)
+	if (probability(0.01 *( (msg.content.length / 50)+1) * (sinceLast / 3) / (spam*2))) {
     msg.channel.send(":potato:")
     sinceLast = 0
   } else { sinceLast++ }
