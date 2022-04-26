a5c6de313476088a61f0cbb5466e0ebbc638ec2c
RedGuy7 <>
2022-04-26 17:59:51 +0000

    remove console.logs i added

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,5 +1,4 @@
 function probability(n){
-	console.log(n)
   return Math.random() < n;
 }
 
@@ -10,7 +9,6 @@ async function handler(msg) {
   let messages = (await msg.channel.messages.fetch({limit: 6})).toJSON();
   let spam = messages.filter((e) => e.author.bot || e.author.id == msg.author.id).length+1
 
-	console.log(msg.content.length,sinceLast,spam)
 	if (probability(0.01 *( (msg.content.length / 50)+1) * (sinceLast / 3) / (spam*2))) {
     msg.channel.send(":potato:")
     sinceLast = 0
