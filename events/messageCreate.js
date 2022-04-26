389690652713a452a1c08416b4fd1b4df2355e25
RedGuy12 <paul@reid-family.org>
2022-04-26 10:05:51 -0500

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,15 +2,16 @@ function probability(n){
   return Math.random() < n;
 }
 
+let sinceLast = 0
+
 async function handler(msg) {
   if(msg.author.bot) return;
-  let messages = Array.from((await msg.channel.messages.fetch({limit: 6})).values());
-  let spam = messages.every((e)=>{
-    return e.author.id == msg.author.id
-  })
-  if (probability(0.05) && msg.content.length >= 5 && !spam) {
+  let messages = (await msg.channel.messages.fetch({limit: 10})).toJSON();
+  let spam = messages.filter((e) => e.author.bot || e.author.id == msg.author.id).length + 1
+  if (probability(0.05 * (msg.content.length / 5) * (sinceLast / 2) / spam)) {
     msg.channel.send(":potato:")
-  }
+    sinceLast = 0
+  } else { sinceLast++ }
 }
 
 module.exports={
